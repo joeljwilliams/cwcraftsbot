155f9bb4ba6b0dea16c548487f68af07403803a1
Joel <joel@yoloswag.in>
2018-04-07 16:47:07 +0200

    Fix environment variable issues for OpenShift deployment

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 import os
 
 TOKEN = os.getenv('BOT_TOKEN')
-ENV = os.getenv('ENV')
+APP_ENV = os.getenv('APP_ENV')
 
 DB_USER = os.getenv('DATABASE_USER')
 DB_PASS = os.getenv('DATABASE_PASSWORD')
 DB_HOST = os.getenv('DATABASE_HOSTNAME')
 DB_NAME = os.getenv('DATABASE_NAME')
 
-if ENV == 'PROD':
+if APP_ENV == 'PROD':
     LOGLEVEL = 'INFO'
 else:
     LOGLEVEL = 'DEBUG'
